17e96bfee7228a97f8b9cbd6a96c5d2bc435b3db
[DaniArango] <[daniarangop93@gmail.com]>
2022-10-26 09:11:27 +0200

    segunda parte de ejercicio

--- Ej3/index.js
+++ Ej3/index.js
@@ -1,10 +1,6 @@
 const Logger = require('logplease');
 const logger = Logger.create('utils');
 const numeros = require ('./numeros');
-const http = require('http');
-const url = require('url');
-const fs = require ('fs');
-
 
 const numeros = [2, 3, 101, 201, 202, 100];
 
@@ -15,24 +11,5 @@ for (const num of numeros) {
         logger.error(`El numero ${num} no es par`)
 }
 
-
-
-http.createServer((req, res) => {
-    const q = url.parse(req.url, true);
-    const filename = "./pages" + q.pathname + ".html";
-    console.log(filename)
-    fs.readFile(filename, (err, data) => {
-      try {
-        res.writeHead(200, { "Content-Type": "text/html" });
-        res.write(data);
-        return res.end();
-      } catch (error) {
-        fs.readFile("./pages/inicio.html", (err, data) => {
-        res.writeHead(404, { "Content-Type": "text/html" });
-        console.error(error);
-        return res.end(data);
-      })}
-    });
-  }).listen(8080);
 
 
